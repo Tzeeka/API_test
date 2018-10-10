90292e72e14b7440d7cbb48252cc4a9307fd8038
andrey.viktorov <andrey.viktorov@binomo.com>
2018-10-10 18:02:23 +0300

    создал pojo через jsonPath

--- src/test/java/pack1/RestAssuredTestArticle.java
+++ src/test/java/pack1/RestAssuredTestArticle.java
@@ -1,14 +1,19 @@
 package pack1;
 
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.core.Is.is;
 
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.json.JSONException;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import source_pack.*;
+import source_pack.ResponseClass;
+import source_pack.Translations;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,11 +21,32 @@ import java.util.List;
 
 public class RestAssuredTestArticle {
 
+    public static String ENDPOINT = "http://restcountries.eu/rest/v1/name/russia";
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = "http://restcountries.eu/rest/v1";
+
+    }
+
     @Test
     public void getRequestExampleTest() throws JSONException {
-        Response response = get("http://restcountries.eu/rest/v1/name/russia");
-        List<ResponseClass> message = Arrays.asList(response.getBody().as(ResponseClass[].class));
-        Assert.assertThat(message.get(0).translations.de, is("Russland"));
+        Response response = get(ENDPOINT);
+        /*List<ResponseClass> message = Arrays.asList(response.getBody().as(ResponseClass[].class));*/
+
+        JsonPath jsonPath = RestAssured.given().
+                when().
+                get(ENDPOINT).
+                then().
+                assertThat().
+                extract().body().jsonPath();
+
+       /* List<ResponseClass> message = Arrays.asList(response.getBody().as(ResponseClass[].class));*/
+
+        List<ResponseClass> message2 = jsonPath.getList("", ResponseClass.class);
+
+
+        Assert.assertThat(message2.get(0).translations.de, is("Russland"));
         Assert.assertThat(response.getStatusCode(), is(200));
     }
 }
